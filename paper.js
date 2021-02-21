3cc3f5504f4068732d74e3f75a7d7d39859cfdd2
zeal2407 <77246154+zeal2407@users.noreply.github.com>
2021-02-20 23:06:04 -0500

    Update paper.js

--- paper.js
+++ paper.js
@@ -4,7 +4,7 @@ class Paper {
         isStatic: false,
         'restitution':0.3,
         'friction':0.5,
-        'density':1.2
+        'density':2.5
       }
       this.body = Bodies.circle(x, y, radius, options);
       this.radius = radius
