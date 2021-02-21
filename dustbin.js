a4e7aa6af32ec82bb6f1cf79da1652e7bdcd2e40
zeal2407 <77246154+zeal2407@users.noreply.github.com>
2021-02-20 23:06:50 -0500

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -1,9 +1,9 @@
 class Dustbin{
     constructor(x, y, width, height) {
-        var options = {
-            'restitution':0.8,
+        //var options = {
+          //  'restitution':0.8,
             
-        }
+        //}
         this.body = Bodies.rectangle(x, y,width, height, options);
         this.width = width;
         this.height = height;
